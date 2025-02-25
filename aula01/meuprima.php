52aff67d26005c276a55782d5daedac434979dc0
Guilherme Ponciano <guilherme.miranda251114@gmail.com>
2025-02-25 16:32:31 -0300

    Aulka de PHP com exemplos de concatenação de variáveis

---
+++ aula01/meuprima.php
@@ -0,0 +1,13 @@
+<?php
+echo "Olá Mundo!";
+
+$vscode = "Jrgwgr";
+$s ="onjgd";
+$nome ="GUI";
+
+echo "$vscode";
+echo $vscode . "<p>" . $s . "<p>" . $nome . "<p>";
+
+
+?>
+<!-- auihfdbihuooiyhbfseiujhbfsae -->
